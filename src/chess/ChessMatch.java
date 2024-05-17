97c5a66ffebfeee41626d8cfbdb22050745c0346
Murilo Nunes <murilosouza.nunes@gmail.com>
2024-05-17 19:19:13 -0300

    Simulate possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -49,6 +49,8 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
             throw new ChessException("There is no piece on source position");
+        } else if (!board.piece(position).isThereAnyPossibleMove()) {
+            throw new ChessException("There is no possible moves for the chosen piece");
         }
     }
 
